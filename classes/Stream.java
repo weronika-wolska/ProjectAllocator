4dd31be8edbce55eeb1b680a3ff96cfa1255d010
unknown <weronika.wolska02@gmail.com>
2020-03-09 16:00:25 +0000

    Stream enum and Student class added

---
+++ classes/Stream.java
@@ -0,0 +1,6 @@
+package classes;
+
+public enum Stream{
+    CS,
+    DS
+}
---
+++ classes/Student.java
@@ -0,0 +1,49 @@
+package classes;
+
+
+public class Student{
+    private String firstName;
+    private String surname;
+    private Long studentId;
+    private Stream stream;
+
+    public Student(String firstName, String surname, Long studentId, Stream stream){
+        this.firstName=firstName;
+        this.surname=surname;
+        this.studentId=studentId;
+        this.stream=stream;
+    }
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public Long getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(Long studentId) {
+        this.studentId = studentId;
+    }
+
+    public Stream getStream() {
+        return stream;
+    }
+
+    public void setStream(Stream stream) {
+        this.stream = stream;
+    }
+}
