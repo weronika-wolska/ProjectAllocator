ccf7304d65f0fc2a5ec838ec8384e3865e1be04c
Lucie <lucie.konecna@ucdconnect.ie>
2020-03-11 05:04:49 +0000

    implemented writing of projects to spreadsheet

--- Test_Sets/Project_Sets/src/main/java/application/CreateProjectSetsApp.java
+++ Test_Sets/Project_Sets/src/main/java/application/CreateProjectSetsApp.java
@@ -1,5 +1,7 @@
 package application;
 
+import entities.Project;
+import entities.ProjectWriter;
 import entities.StaffMember;
 import entities.StaffReader;
 
@@ -10,9 +12,11 @@ public class CreateProjectSetsApp {
     public static void main(String[] args) {
         StaffReader reader = new StaffReader();
         reader.readXLSX(30, "src/main/resources/staff.xlsx");
-
         for (StaffMember staff: reader.getFaculty()) {
             System.out.println(staff.toString());
         }
+
+        ProjectWriter writer = new ProjectWriter();
+        writer.write("src/main/resources/projects.xlsx",reader.getFaculty());
     }
 }
--- Test_Sets/Project_Sets/src/main/java/entities/ProjectWriter.java
+++ Test_Sets/Project_Sets/src/main/java/entities/ProjectWriter.java
@@ -1,4 +1,60 @@
 package entities;
 
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.ArrayList;
+
 public class ProjectWriter {
+
+    public void write(String filePath, ArrayList<StaffMember> faculty) {
+        XSSFWorkbook workbook = new XSSFWorkbook();
+        XSSFSheet sheet = workbook.createSheet("Projects and Supervisors");
+        createHeader(sheet);
+        int rowNum = 1;
+        for (StaffMember staff :
+                faculty) {
+            for (String projectTitle :
+                    staff.getProjects()) {
+                Row row = sheet.createRow(rowNum);
+                writeRow(row, staff, projectTitle);
+                ++rowNum;
+            }
+        }
+        try {
+            FileOutputStream outputFile = new FileOutputStream(new File(filePath));
+            workbook.write(outputFile);
+            outputFile.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void writeRow(Row row, StaffMember staff, String project) {
+        Cell cell = row.createCell(0);
+        cell.setCellValue(staff.getName());
+        cell = row.createCell(1);
+        cell.setCellValue(project);
+        cell = row.createCell(2);
+        if(staff.getSpecialFocus() == Stream.DS) {
+            cell.setCellValue("Dagon Studies");
+        }
+        else {
+            cell.setCellValue((String) null);
+        }
+    }
+
+    private void createHeader(XSSFSheet sheet) {
+        Row row = sheet.createRow(0);
+        Cell cell = row.createCell(0);
+        cell.setCellValue("Supervisor");
+        cell = row.createCell(1);
+        cell.setCellValue("Project");
+        cell = row.createCell(2);
+        cell.setCellValue("Special Focus");
+    }
 }
